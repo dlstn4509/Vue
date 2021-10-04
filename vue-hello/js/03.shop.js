549d8e3265d686e8e9ba2f6d5c40442d3b7b85ce
dlstn4509 <dlstn7609@hanmail.net>
2021-10-04 14:37:36 +0900

    03 shop 완

--- vue-hello/js/03.shop.js
+++ vue-hello/js/03.shop.js
@@ -2,12 +2,25 @@ new Vue({
 	el: '#app',
 	data: {
 		products: [],
+		searchProducts: [],
+		query: ''
 	},
 	async created() {
 		const {data} = await axios.get('../json/foods.json')
 		this.products = data
+		this.searchProducts = data
 	},
 	methods: {
-
+		onReset(e) {
+			this.query = ''
+			this.searchProducts = this.products
+			document.querySelector('.search-wrap input').focus()
+		},
+		onSearch(e) {
+			this.query = e.target.value
+			this.searchProducts = this.products.filter(v => {
+				return v.title.toLowerCase().includes(this.query.toLowerCase()) || v.description.toLowerCase().includes(this.query.toLowerCase())
+			})
+		}
 	}
 })
