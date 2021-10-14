fb4737bbda11c6796958e2291c67a0f8168a95e1
dlstn4509 <dlstn7609@hanmail.net>
2021-10-14 15:31:27 +0900

    views 완

---
+++ vue-book/src/views/ListInfinite.vue
@@ -0,0 +1,12 @@
+<template>
+  <InfiniteCp />
+</template>
+
+<script>
+import InfiniteCp from "../components/InfiniteCp.vue";
+
+export default {
+  name: "Infinite",
+  components: { InfiniteCp },
+};
+</script>
