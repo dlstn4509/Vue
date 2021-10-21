f63a287f5e6134334cf3074745e737b937128ad6
dlstn4509 <dlstn7609@hanmail.net>
2021-10-21 11:57:35 +0900

    ..

--- vue-book/src/api/index.js
+++ vue-book/src/api/index.js
@@ -7,6 +7,7 @@ import axios from "axios";
 const { VUE_APP_API_KEY, VUE_APP_URL_BOOKS, VUE_APP_URL_VIEW } = process.env;
 
 const apiBooks = (page, { listCnt, pagerCnt }) => {
+  // http://127.0.0.1:3100/book/10?apikey=c9e5c72a-01ab-4c6f-bf1e-dd85f9154e65&listCnt=10&pagerCnt=10
   return axios.get(VUE_APP_URL_BOOKS + "/" + page, {
     params: { apikey: VUE_APP_API_KEY, listCnt, pagerCnt },
     withCredentials: true,
